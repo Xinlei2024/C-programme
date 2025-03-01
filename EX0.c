a6e5313e2058f0ea9d9e317f310bae0ef77dabe9
zxl819 <79512237+zxl819@users.noreply.github.com>
2025-03-01 12:39:29 +0800

    Learn C in a hard way
    学习C语言的编译过程

---
+++ EX0.c
@@ -0,0 +1,15 @@
+//
+//  EX0.swift
+//  C programme
+//
+//  Created by zxl on 2025/2/28.
+//
+
+#include <stdio.h>
+int main(int argc,char *argv[])
+{
+    puts("Hello World. Hello xl.");
+    printf("Hello World.Hello");
+    
+    return 0;
+}
---
+++ Makefile
@@ -0,0 +1,6 @@
+CFLAGS=-Wall -g
+all：
+	EX0 EX3
+clean:
+	rm -f EX0 
+	rm -f EX3
